24e2ce4d11a51fc7f51e9004226d6c3c7e944c69
Adam Beadle <adambeadle@Adams-MacBook-Air.local>
2026-08-30 18:04:58 +0800

    Refine FORBAIR icon scale

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='forbair-v2-charcoal-brand';
+const CACHE='forbair-v3-midpoint-brand';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/','/brand/forbair-app-icon-charcoal.png']))));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('/'))))});
