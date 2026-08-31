cf3faad753085de0fbb38000ccdc6f7bf9dcb60d
Adam Beadle <adambeadle@Adams-MacBook-Air.local>
2026-08-31 21:40:03 +0800

    Unify Log modal design system

--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,7 @@ import './sleep.css';
 import './activity.css';
 import './nutrition.css';
 import './money.css';
+import './universal-log.css';
 import WorkoutModule from './WorkoutModule';
 import GoalsModule from './GoalsModule';
 import SettingsModule from './SettingsModule';
