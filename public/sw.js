2dfd08e8f161823ab9fbbeb363a6d9d324b56e80
Adam Beadle <adambeadle@Adams-MacBook-Air.local>
2026-09-02 10:53:51 +0800

    Add Fovyn browser favicon assets

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='fovyn-v5-grow-tagline';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/','/brand/forbair-app-icon-charcoal.png']))));
+const CACHE='fovyn-v6-favicon';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/','/brand/forbair-app-icon-charcoal.png','/brand/favicon/favicon-16.png','/brand/favicon/favicon-32.png','/brand/favicon/favicon-48.png']))));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('/'))))});
